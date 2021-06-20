8ce5c99019c83b9b10c5efb44e85a0a4a73bf6a5
Sohail Mahmud <sohailmahmud@yahoo.com>
2021-06-20 11:03:34 +0600

    Create Db5Model.dart

---
+++ lib/dashboard/model/db5/Db5Model.dart
@@ -0,0 +1,30 @@
+class Db5CategoryData {
+  var name = "";
+  var image = "";
+}
+
+class Db6BestDestinationData {
+  var name = "";
+  var rating = "";
+  var image = "";
+}
+
+class Db6RecentSearchData {
+  var name = "";
+}
+
+class Db6BestHotelData {
+  var name = "";
+  var rating = "";
+  var image = "";
+}
+
+class Db6HotelData {
+  var name = "";
+  var rating = "";
+  var address = "";
+  var room_detail = "";
+  var hotel_review = "";
+  var image = "";
+  var price = "";
+}
