d26a019cfe4724f7661949d2e63bb7a969555187
Sohail Mahmud <sohailmahmud@yahoo.com>
2021-06-20 13:57:09 +0600

    Create Db5Widget.dart

---
+++ lib/dashboard/utils/Db5Widget.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:prokit_flutter/dashboard/utils/DbColors.dart';
+
+import 'DbConstant.dart';
+
+Widget text(String text,
+    {var fontSize = textSizeMedium,
+    textColor = db5_black,
+    var fontFamily = fontRegular,
+    var isCentered = false,
+    var maxLine = 1,
+    var latterSpacing = 0.25,
+    var textAllCaps = false,
+    var isLongText = false}) {
+  return Text(textAllCaps ? text.toUpperCase() : text,
+      textAlign: isCentered ? TextAlign.center : TextAlign.start,
+      maxLines: isLongText ? null : maxLine,
+      style: TextStyle(fontFamily: fontFamily, fontSize: fontSize, color: textColor, height: 1.5, letterSpacing: latterSpacing));
+}
+
+BoxDecoration boxDecoration({double radius = 2, Color color = Colors.transparent, Color bgColor = db5_white, var showShadow = false}) {
+  return BoxDecoration(
+      color: bgColor,
+      //gradient: LinearGradient(colors: [bgColor, whiteColor]),
+      boxShadow: showShadow ? [BoxShadow(color: dbShadowColor, blurRadius: 10, spreadRadius: 2)] : [BoxShadow(color: Colors.transparent)],
+      border: Border.all(color: color),
+      borderRadius: BorderRadius.all(Radius.circular(radius)));
+}
