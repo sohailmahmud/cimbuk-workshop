7695c081fc720c54db977e197e352b0eb374870c
Sohail Mahmud <sohailmahmud@yahoo.com>
2021-06-20 13:56:39 +0600

    Create Db2Widget.dart

---
+++ lib/dashboard/utils/Db2Widget.dart
@@ -0,0 +1,58 @@
+import 'package:flutter/material.dart';
+import 'package:prokit_flutter/dashboard/utils/DbColors.dart';
+
+import 'DbConstant.dart';
+import 'DbStrings.dart';
+
+Widget text(String text,
+    {var fontSize = textSizeMedium,
+    textColor = db2_textColorPrimary,
+    var fontFamily = fontRegular,
+    var isCentered = false,
+    var maxLine = 1,
+    var latterSpacing = 0.25,
+    var textAllCaps = false,
+    var isLongText = false}) {
+  return Text(textAllCaps ? text.toUpperCase() : text,
+      textAlign: isCentered ? TextAlign.center : TextAlign.start,
+      maxLines: isLongText ? null : maxLine,
+      style: TextStyle(fontFamily: fontFamily, fontSize: fontSize, color: textColor, height: 1.5, letterSpacing: latterSpacing));
+}
+
+BoxDecoration boxDecoration({double radius = 2, Color color = Colors.transparent, Color bgColor = db2_white, var showShadow = false}) {
+  return BoxDecoration(
+      color: bgColor,
+      //gradient: LinearGradient(colors: [bgColor, whiteColor]),
+      boxShadow: showShadow ? [BoxShadow(color: dbShadowColor, blurRadius: 10, spreadRadius: 2)] : [BoxShadow(color: Colors.transparent)],
+      border: Border.all(color: color),
+      borderRadius: BorderRadius.all(Radius.circular(radius)));
+}
+
+Widget db2Label(var text) {
+  return Padding(
+    padding: const EdgeInsets.all(16.0),
+    child: Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: <Widget>[
+        Text(text, style: TextStyle(fontSize: textSizeNormal, color: db2_textColorPrimary, fontFamily: fontMedium)),
+        RichText(
+          text: TextSpan(
+            children: [
+              TextSpan(text: db2_lbl_view_all, style: TextStyle(fontSize: textSizeSMedium, color: db2_textColorSecondary)),
+              WidgetSpan(
+                child: Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 4.0),
+                  child: Icon(
+                    Icons.keyboard_arrow_right,
+                    color: db2_textColorSecondary,
+                    size: 16,
+                  ),
+                ),
+              ),
+            ],
+          ),
+        )
+      ],
+    ),
+  );
+}
